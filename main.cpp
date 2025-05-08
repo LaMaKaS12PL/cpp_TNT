e9a867a3d5c4513ce27f24c9d7157040f51a048a
LaMaKaS12PL <145048328+LaMaKaS12PL@users.noreply.github.com>
2025-05-08 06:45:11 +0000

    funkcje dodalem

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+void my_print(const string &text)
+{
+    cout << text << endl;
+}
+
+void menu(string &text)
+{
+    my_print("Podaj tekst");
+    getline(cin, text);
+}
+
+int main()
+{
+    string text;
+
+    my_print("Ala ma kota");
+    menu(text);
+    string newText = "Nowy tekst = " + text;
+    my_print(newText);
+
+    return 0;
+}
